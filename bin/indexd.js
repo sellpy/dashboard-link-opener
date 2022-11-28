2c5aab9dae04fdb73ad1de17fd9b0cfaa3d879b9
Julian Lundberg <julian.lundberg@sellpy.se>
2022-11-28 14:34:00 +0100

    Implemented support for dashd

--- bin/indexd.js
+++ bin/indexd.js
@@ -2,6 +2,6 @@
 const { main } = require('../lib/link-opener')
 
 const [_, __, className, field, constraint, compareTo] = process.argv
-const BASE_URL = 'https://dashboard.sellpy.net/apps/Sellpy%20Prod%20-%20Read%20Only/browser'
+const BASE_URL = 'https://dashboard.sellpy.net/apps/Sellpy%20Dev/browser'
 
 main(BASE_URL, className, field, constraint, compareTo)
